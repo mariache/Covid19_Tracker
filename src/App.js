9b65400e5748288744563ddffbf73ce233475bf5
Salam Omarbagaev <omarbagaev@gmail.com>
2020-08-14 23:56:40 +1200

    - converted App.js to a function;

--- src/App.js
+++ src/App.js
@@ -1,46 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./App.module.css";
 import { Cards, Chart, CountryPicker, Footer } from "./components";
 import { fetchData } from "./api";
 import image from "./images/covid19.png";
 
-class App extends React.Component {
-  state = {
-    data: {},
-    country: "",
-  };
+const App = () => {
+  const [data, setData] = useState({});
+  const [country, setCountry] = useState("");
 
-  async componentDidMount() {
-    const fetchedData = await fetchData();
-    this.setState({ data: fetchedData });
-  }
+  useEffect(() => {
+    const fetchedData = async () => {
+      setData(await fetchData());
+    };
+    fetchedData();
+  }, []);
 
-  handleCountryChange = async (country) => {
+  const handleCountryChange = async (country) => {
     const fetchedData = await fetchData(country);
-    this.setState({ data: fetchedData, country: country });
+    setCountry(country);
+    setData(fetchedData);
   };
 
-  render() {
-    const { data, country } = this.state;
-    return (
-      <div className={styles.container}>
-        <div className={styles.app}>
-          <div className={styles.app__left}>
-            <div className={styles.app__header}>
-              <img className={styles.image} src={image} alt="COVID-19" />
-              <CountryPicker handleCountryChange={this.handleCountryChange} />
-            </div>
+  return (
+    <div className={styles.container}>
+      <div className={styles.app}>
+        <div className={styles.app__left}>
+          <div className={styles.app__header}>
+            <img className={styles.image} src={image} alt="COVID-19" />
+            <CountryPicker handleCountryChange={handleCountryChange} />
           </div>
         </div>
-        <h1 className={styles.header}>
-          {country ? `Live stats for ${country}` : "Live stats worldwide"}
-        </h1>
-        <Cards data={data} />
-        <Chart country={country} data={data} />
-        <Footer data={data} />
       </div>
-    );
-  }
-}
+      <h1 className={styles.header}>
+        {country ? `Live stats for ${country}` : "Live stats Worldwide"}
+      </h1>
+      <Cards data={data} />
+      <Chart country={country} data={data} />
+      <Footer data={data} />
+    </div>
+  );
+};
 
 export default App;
